195cadc8904566a4007f715620dc6c8ab5d44fbf
Mar Jene <mar.jene@somenergia.coop>
2019-11-21 09:16:08 +0100

    alexandra.catalog_test.Catalog_Test.test_book_byDefault

---
+++ alexandra/catalog.py
@@ -0,0 +1,13 @@
+class Catalog(object):
+
+
+    def description(self):
+        return """\
+# Alexandra's Catalog
+
+No books available at the moment
+"""
+
+
+    def book(self):
+        return None
---
+++ alexandra/catalog_test.py
@@ -0,0 +1,34 @@
+import unittest
+from .catalog import Catalog
+from somutils import testutils
+from .book import Book
+class Catalog_Test(unittest.TestCase):
+
+    def test_description_empty(self):
+        catalog = Catalog()
+        description = catalog.description()
+        self.assertEquals(description, """\
+# Alexandra's Catalog
+
+No books available at the moment
+""")
+
+    def _test_description_oneBook(self):
+        catalog = Catalog()
+        book = Book()
+        catalog.addBook(book)
+        self.assertEqual(catalog.description(), """\
+# Alexandra's Catalog
+
+- {}
+""".format(book.description()))
+
+    def _test_addBook(self):
+        catalog = Catalog()
+        book = Book()
+        catalog.addBook(book)
+        self.assertEqual(catalog.book(), book)
+
+    def test_book_byDefault(self):
+        catalog = Catalog()
+        self.assertEqual(catalog.book(), None)
